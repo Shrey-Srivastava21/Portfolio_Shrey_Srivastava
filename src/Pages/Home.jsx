20f9b349cc7b7a51c0d2a7158cd979b909e2574e
Shrey Srivastava <devshiva.1738@gmail.com>
2023-05-05 03:32:31 +0530

    Portfolio Update

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,297 @@
+import { Box, Button, Center, Flex, Heading, HStack, Link, Tooltip, Image, Text } from '@chakra-ui/react'
+import { useToast } from '@chakra-ui/react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import Aos from 'aos';
+import 'aos/dist/aos.css'
+import GitHubCalendar from 'react-github-calendar';
+import ReactTooltip from 'react-tooltip';
+import { useEffect } from 'react';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { GoCloudDownload } from 'react-icons/go'
+import { FaPhoneAlt } from 'react-icons/fa'
+import { SiGmail } from 'react-icons/si'
+
+import { projects, skills } from '../Utils/data';
+
+import ProjectCard from '../Components/Card';
+import Svg1 from '../Components/Svg1';
+import Svg2 from '../Components/Svg2';
+import Svg3 from '../Components/Svg3';
+import Slider from 'react-slick';
+import Resume from '../Resume/Shrey-Srivastava-Resume.pdf'
+
+const Home = () => {
+
+    const form = useRef();
+    const toast = useToast()
+
+    const settings = {
+        dots: false,
+        infinite: false,
+        slidesToShow: matchMedia("(max-width: 425px)").matches ? 1 : matchMedia("(max-width: 1024px)").matches ? 2 : 3,
+        slidesToScroll: 1,
+    };
+
+    useEffect(() => {
+        // * it's from Aos library to to use scroll designing
+        Aos.init()
+    }, [])
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_ot57meh', 'template_82m6hki', form.current, 's7qxzSHyvTMO1qNhD').then((result) => {
+            console.log(result.text);
+            toast({
+                position: 'top-right',
+                title: 'Email Sent ✔',
+                description: `Thank You ${form.current.from_name.value.split(" ")[0]} for the message!`,
+                status: 'success',
+                duration: 5000,
+                isClosable: true,
+            })
+            form.current.from_name.value = "";
+            form.current.user_email.value = "";
+            form.current.message.value = "";
+        }, (error) => {
+            console.log(error.text);
+            toast({
+                title: 'Email Not sent.',
+                description: "There is some error",
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+            })
+        });;
+
+    };
+
+
+    return (
+        <Box>
+            <Box id='home'>
+                <Flex flexDirection={['column-reverse', 'column-reverse', 'row']} m="auto" justifyContent="space-around" alignItems="center" h="100%">
+                    <Box data-aos="fade-down">
+                        <Heading>Hey! <span className='themeText'>I'm</span></Heading>
+                        <Box className='content'>
+                            <Heading fontSize="3.3em" className='text' data-text="Shrey Srivastava"><span className='themeText'>Shrey Srivastava</span></Heading>
+                        </Box>
+                        <Text>Am a Software Developer passionate and experienced in building Web applications.</Text>
+                        <HStack className='hireMe' onClick={() => { window.open("https://1drv.ms/b/s!AjfSNL-3_Mfng0dVyp7QsLXQT92h?e=uncCFn", '_blank') }}>
+                            <a href={Resume} download="Shrey-Srivastava-Resume">
+                                <Button>Resume <GoCloudDownload /></Button>
+                            </a>
+                        </HStack>
+                    </Box>
+                    <Box data-aos="fade-down">
+                        <Svg1 />
+                    </Box>
+                </Flex>
+            </Box>
+
+            {/* About me */}
+
+            <Box id="aboutMe">
+                <Heading>About <span className='themeText'>me</span></Heading>
+                <Flex flexDirection={['column-reverse', 'column-reverse', 'column-reverse', 'row']} alignItems="center" h="100%">
+                    <div data-aos="fade-right">
+                        <Svg3 />
+                    </div>
+
+                    <Flex data-aos="fade-left">
+                        <Flex w="100%" gap="10%" justifyContent="center">
+                            <Image
+                                borderRadius='full'
+                                boxSize='250px'
+                                src='https://user-images.githubusercontent.com/84815622/236336018-c67135d5-c818-4612-b90a-a2800eefbd54.jpeg'
+                                alt='Dan Abramov' />
+
+                            <Svg3 />
+                        </Flex>
+
+                        <Box>
+                            <Text>A 3rd year B.Tech student at VIT Bhopal University, an ambitious and self-motivated webdeveloper with a considerable technical skill who possesses self-discipline and ability to work with minimum of super vision.Able to play a key role in website development to ensure maximum accessibility,user experience and usability.A  quick-learner who can absorb new ideas and can communicate clearly and effectively. Have 1200+ hours of coding experience and solved around 600+ DSA problems.</Text>
+                        </Box>
+                    </Flex>
+                </Flex>
+            </Box>
+
+            {/* Technical Skills section */}
+            <Box id="skills">
+                <Heading>
+                    Technical
+                    <span className="themeText"> Skills</span>
+                </Heading>
+                <Flex className='skills'>
+                    <Flex>
+                        <Heading size="lg">Front<span className='themeText'>end</span></Heading>
+                        <Box>
+                            {
+                                skills.filter((el) => el.tag === "frontend").map(skill => <Box
+                                    key={skill.id}
+                                    className="skill"
+                                    data-aos="zoom-in-up">
+                                    <Box>
+                                        <Image src={skill.icon} />
+                                    </Box>
+                                    <Text>{skill.title}</Text>
+                                </Box>)
+                            }
+                        </Box>
+                    </Flex>
+                    <Flex>
+                        <Heading size="lg">Back<span className='themeText'>end</span></Heading>
+                        <Box>
+                            {
+                                skills.filter((el) => el.tag === "backend").map(skill => <Box
+                                    key={skill.id}
+                                    className="skill"
+                                    data-aos="zoom-in-down">
+                                    <Box>
+                                        <Image src={skill.icon} />
+                                    </Box>
+                                    <Text>{skill.title}</Text>
+                                </Box>)
+                            }
+                        </Box>
+                    </Flex>
+                    <Flex>
+                        <Heading size="lg">Platforms <span className='themeText'>& Tools</span></Heading>
+                        <Box>
+                            {
+                                skills.filter((el) => el.tag === "platform").map(skill => <Box
+                                    key={skill.id} className="skill"
+                                    data-aos="zoom-in">
+                                    <Box>
+                                        <Image src={skill.icon} />
+                                    </Box>
+                                    <Text>{skill.title}</Text>
+                                </Box>)
+                            }
+                        </Box>
+                    </Flex>
+                </Flex>
+            </Box>
+
+
+            {/* show projects */}
+            <Box id="projects">
+                <Heading textAlign="center">Featured <span className='themeText'>Projects</span></Heading>
+                <Slider {...settings}>
+                    {
+                        projects.map((project, i) => <ProjectCard key={i} {...project} />)
+                    }
+                </Slider>
+            </Box>
+
+
+            {/* Github Statistics */}
+            <Box id="githubStats">
+                <Heading textAlign="center">Github <span className='themeText'>stats</span></Heading>
+                <Center>
+                    <Image src="https://github-readme-stats.vercel.app/api/top-langs/?username=Shrey-Srivastava21&layout=compact&hide_border=true&theme=radical" alt="Most used languages" />
+                </Center>
+
+                <Center className='github-stats'>
+                    <Image src="https://github-readme-stats.vercel.app/api?username=Shrey-Srivastava21&show_icons=true&locale=en&layout=compact&hide_border=true&theme=radical" alt="Github stats" />
+                    <Image src="https://github-readme-streak-stats.herokuapp.com/?user=Shrey-Srivastava21&layout=compact&hide_border=true&theme=radical" alt="Current Streaks" />
+                </Center>
+
+                <Center>
+                    <GitHubCalendar username="Shrey-Srivastava21" color="#4a8af4" children={<ReactTooltip html />} />
+                </Center>
+            </Box>
+
+
+            {/* Contact me */}
+            <Box id='contactMe'>
+                <Heading textAlign="center">Contact <span className='themeText'>Me</span></Heading>
+                <Flex flexDirection={["column", "column", "column", "row"]} alignItems="center">
+
+                    <Box>
+                        <Svg2 />
+                    </Box>
+
+
+                    <Box className='form-section'>
+                        <form ref={form} onSubmit={sendEmail}>
+                            <div className='inputBox'>
+                                <input type="text" name="from_name" required />
+                                <span>Full Name</span>
+                            </div>
+                            <div className='inputBox'>
+                                <input type="email" name="user_email" required />
+                                <span>Email</span>
+                            </div>
+                            <div>
+                                <textarea placeholder='Message 📧' name="message" />
+                            </div>
+                            <input type="submit" value="Send Message" />
+                        </form>
+                        <Flex className='contact-info'>
+                            <HStack>
+                                <SiGmail color="#e34133" />
+                                <Text>demonshrey.1738@gmail.com</Text>
+                            </HStack>
+                            <HStack>
+                                <FaPhoneAlt color="#00a14f" />
+                                <Text>+91 63920 98133</Text>
+                            </HStack>
+                        </Flex>
+                        <Flex gap={["10px", "20px", "20px", "40px"]}>
+                            <Link href='https://wa.me/918101303481' target="_blank">
+                                <Tooltip label='+91 81013 03481'>
+                                    <Box className='social-icons'>
+                                        <Box>
+                                            <Image w="100%" src="https://brandlogos.net/wp-content/uploads/2018/10/whatsapp-logo.png" />
+                                        </Box>
+                                    </Box>
+                                </Tooltip>
+                            </Link>
+
+                            <Link href='https://www.linkedin.com/in/shrey-srivastava21/' target="_blank">
+                                <Tooltip label='Shrey Srivastava'>
+                                    <Box className='social-icons'>
+                                        <Box>
+                                            <Image w="100%" src="https://openvisualfx.com/wp-content/uploads/2019/10/linkedin-icon-logo-png-transparent.png" />
+                                        </Box>
+                                    </Box>
+                                </Tooltip>
+                            </Link>
+
+                            <Link href="https://github.com/Shrey-Srivastava21" target="_blank">
+                                <Tooltip label='Shrey-Srivastava21'>
+                                    <Box className='social-icons'>
+                                        <Box>
+                                            <Image w="100%" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" />
+                                        </Box>
+                                    </Box>
+                                </Tooltip>
+                            </Link>
+
+                            <Link href="mailto:demonshrey.1738@gmail.com" target="_blank">
+                                <Tooltip label='demonshrey.1738@gmail.com'>
+                                    <Box className='social-icons'>
+                                        <Box>
+                                            <Image w="100%" src="https://1000logos.net/wp-content/uploads/2021/05/Gmail-logo.png" />
+                                        </Box>
+                                    </Box >
+                                </Tooltip>
+                            </Link>
+                        </Flex >
+                    </Box >
+                </Flex >
+            </Box >
+
+            {/* footer */}
+            <Flex id='footer'>
+                <Text>© Portfolio by Shrey Srivastava. | All rights reserved.</Text>
+                <Text>Made with 💖 by Shrey Srivastava</Text>
+            </Flex>
+        </Box >
+    )
+}
+
+export default Home
---
+++ src/Utils/data.js
@@ -0,0 +1,181 @@
+export const skills = [
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/174/174854.png",
+        title: "HTML",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+        title: "CSS",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+        title: "JavaScript",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+        title: "JavaScript",
+        tag: "backend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/760/760457.png",
+        title: "react",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968381.png",
+        title: "TypeScript",
+        tag: "backend"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/chakra-ui.png",
+        title: "Chakra-UI",
+        tag: "frontend"
+    },
+    {
+        icon: "https://img.icons8.com/fluency/512/node-js.png",
+        title: "Node.js",
+        tag: "backend"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/nextjs.png",
+        title: "Next.js",
+        tag: "frontend"
+    },
+    {
+        icon: "https://www.svgrepo.com/show/452093/redux.svg",
+        title: "redux",
+        tag: "backend"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/mongodb.png",
+        title: "MongoDB",
+        tag: "backend"
+    },
+    {
+        icon: "https://img.icons8.com/?size=512&id=33039&format=png",
+        title: "AWS",
+        tag: "backend"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/express-js.png",
+        title: "Express.js",
+        tag: "backend"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/bootstrap.png",
+        title: "Bootstrap",
+        tag: "frontend"
+    },
+    {
+        icon: "https://img.icons8.com/ios-glyphs/512/github.png",
+        title: "Github",
+        tag: "platform"
+    },
+    {
+        icon: "https://i.ibb.co/61nck0S/download-removebg-preview.png",
+        title: "VS Code",
+        tag: "platform"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/git.png",
+        title: "Git",
+        tag: "platform"
+    },
+    {
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/New_Replit_Logo.svg/1200px-New_Replit_Logo.svg.png",
+        title: "Replit",
+        tag: "platform"
+    },
+    {
+        icon: "https://img.icons8.com/external-tal-revivo-color-tal-revivo/512/external-codesandbox-an-online-code-editor-and-sharing-web-application-projects-logo-color-tal-revivo.png",
+        title: "Sandbox",
+        tag: "platform"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/firebase.png",
+        title: "Firebase",
+        tag: "platform"
+    },
+    {
+        icon: "https://img.icons8.com/ios-filled/512/codepen.png",
+        title: "CodePen",
+        tag: "platform"
+    },
+
+]
+
+export const projects = [
+    {
+        id: 0,
+        title: 'VITMe',
+        type: 'Dynamic API',
+        image: 'https://user-images.githubusercontent.com/84815622/236336671-7dc3e356-3f6e-4428-b4c3-552896b8d8dd.png',
+        description: "A Dynamic API server for VTOP with Moodle Integration. This project includes cross-platform applications for Android, Windows, and Web.",
+        techStack: ['ReactJs', 'NodeJs', 'ExpressJs', 'MongoDB', 'CSS', 'SCSS', 'HTML', 'Java', 'Python'],
+        github: 'https://github.com/Shrey-Srivastava21/VITMe',
+        liveDemo: 'https://vitme.netlify.app/'
+    },
+    {
+        id: 1,
+        title: "PortfolioBuilder",
+        type: "Portfolio Maker Website",
+        image: "https://user-images.githubusercontent.com/84815622/236337122-0b9ba338-2cf0-42cd-a993-bde77b374f5e.jpeg",
+        description: "Designed and developed a web application that allows developers to create and deploy a professional portfolio website in minutes using their GitHub account.",
+        techStack: ['ReactJs', 'NodeJs', 'ExpressJs', 'MongoDB', 'CSS', 'SCSS', 'HTML', 'Web Socket', 'Postman API'],
+        github: "https://github.com/Shrey-Srivastava21/PortfolioBuilder",
+        liveDemo: "https://portfoliobuilder.netlify.app/"
+    },
+    {
+        id: 2,
+        title: "FFCSAl",
+        type: "App",
+        image: "https://user-images.githubusercontent.com/84815622/236337354-ad591e33-d45c-4ff2-a170-336174b0cf8e.png",
+        description: "FFCSAl is an app to facilitate the smooth and hassle-free preparation of timetables during FFCS. The app can be used by any student studying in VIT Bhopal right from the first years to the final years.",
+        techStack: ['Android', 'Android Studio', 'MongoDB', 'Figma', 'Firebase'],
+        github: "https://github.com/Atanu8250/GadgetRambo",
+        liveDemo: "https://gadget-rambo.vercel.app/"
+    },
+    {
+        id: 3,
+        title: "Hypertube",
+        type: "Movie Recommendation System",
+        image: "https://user-images.githubusercontent.com/84815622/170850248-a661d0f3-107c-45e1-8bec-c22dc1b3d172.png",
+        description: "Movie Recommendation System using ML and MERN for Microsoft Engage-2022.",
+        techStack: ["HTML", "CSS", "JavaScript", "MERN", "ML"],
+        github: "https://github.com/Shrey-Srivastava21/Movie_Recommendation_System",
+        liveDemo: "https://moviemain.herokuapp.com/"
+    },
+    {
+        id: 4,
+        title: "House Prediction Model",
+        type: "PROJECT MODEL",
+        image: "https://user-images.githubusercontent.com/84815622/181276756-3cbb5ca6-8d2a-4468-bc21-0fe797028e94.png",
+        description: "Predictive Analytics Model to predict the House Price.",
+        techStack: ["ML", "Jupyter Notebook"],
+        github: "https://github.com/Shrey-Srivastava21/House_Price_Prediction_Project",
+        liveDemo: "https://ml-house-prediction.netlify.app/"
+    },
+    {
+        id: 5,
+        title: "Fire Detection Model",
+        type: "DETECTION MODEL",
+        image: "https://user-images.githubusercontent.com/84815622/236337979-54c984ae-295c-467c-acdb-472fd98b8629.jpg",
+        description: "Health-Dr. website is used to track your health-diet. Here you can set the goal to have a fitness full body and our website will track your fitness and health",
+        techStack: ["JAVA", "CPP", "Python"],
+        github: "https://github.com/Shrey-Srivastava21/Fire_Detection_System",
+        liveDemo: "https://health-dr-by-team-masai-warriors.netlify.app/"
+    },
+    // {
+    //     id: 6,
+    //     title: "Pablo Parking",
+    //     type: "PARKING WEB APP",
+    //     image: "https://user-images.githubusercontent.com/94675329/212767301-b159c959-43fe-4c9c-8655-93d021829ab5.png",
+    //     description: "This project based on vehicle parking system created based on LLD (Low Level Designing) concept. Here you can book a slot to park your car.",
+    //     techStack: ["HTML", "CSS", "JavaScript"],
+    //     github: "https://github.com/Vishwa9011/PabloParking",
+    //     liveDemo: "https://pabloparking.netlify.app/"
+    // }
+]
